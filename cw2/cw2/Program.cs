b250e6d10eb69e416b928c0c91b9f6cf18ab3a30
Łukasz Rzeczkowski <s18693@pjwstk.edu.pl>
2020-03-09 14:21:50 +0100

    new

---
+++ cw2/cw2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cw2
+{
+    class Program
+    {
+        //Podajemy 3 argumenty
+        //Zrobić do końca piątku
+        //hashSet zeby nie było duplikatów
+        //jaison
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
